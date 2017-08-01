71cbbb88955613e0e63f9257ffe47ad5b9a8238d
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-01 16:53:53 +0200

    Updated test case for new `check` output

--- tests/check_test.go
+++ tests/check_test.go
@@ -14,7 +14,7 @@ func TestCheck(t *testing.T) {
 	ts.initStore()
 	ts.initSecrets("")
 
-	list := `Weak password for fixed/secret: Password is too short`
+	list := `Detected weak password for fixed/secret: Password is too short`
 	out, err := ts.run("check")
 	assert.NoError(t, err)
 	assert.Equal(t, strings.TrimSpace(list), out)
