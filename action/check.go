20a7e68054fb0f393cda974f8cfe0aba58edbf49
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-01 18:10:50 +0200

    `gopass check` now also detects dupe/shared passwords

--- action/check.go
+++ action/check.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/muesli/crunchy"
 	"github.com/urfave/cli"
@@ -19,6 +20,7 @@ func (s *Action) Check(c *cli.Context) error {
 	var out io.Writer
 	out = os.Stdout
 
+	dupes := make(map[string][]string)
 	foundWeakPasswords := false
 	for _, secret := range t.List(0) {
 		content, err := s.Store.Get(secret)
@@ -26,14 +28,22 @@ func (s *Action) Check(c *cli.Context) error {
 			return err
 		}
 
-		if err = crunchy.ValidatePassword(string(content)); err != nil {
+		pw := string(content)
+		if err = crunchy.ValidatePassword(pw); err != nil {
 			foundWeakPasswords = true
 			fmt.Fprintf(out, "Detected weak password for %s: %v\n", secret, err)
 		}
+
+		dupes[pw] = append(dupes[pw], secret)
 	}
 
 	if !foundWeakPasswords {
 		fmt.Fprintln(out, "No weak passwords detected.")
+	}
+	for _, dupe := range dupes {
+		if len(dupe) > 1 {
+			fmt.Fprintf(out, "Detected a shared password for %s\n", strings.Join(dupe, ", "))
+		}
 	}
 
 	return nil
